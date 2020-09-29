1d7582817a0fe0e9944ff6c0c03ac2da23f69e02
brinurmi <ev5443@wayne.edu>
2020-09-29 13:16:24 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import { ActivityIndicator, StyleSheet, StatusBar, View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, {useEffect} from 'react';
 import {createAppContainer, Image} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import {createDrawerNavigator} from 'react-navigation-drawer';
@@ -6,37 +8,41 @@ import {Ionicons} from '@expo/vector-icons';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
-import HealthDashboardScreen from './src/screens/HealthDashboardScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 import DrawerMenu from './src/screens/DrawerMenu';
 
+
+
 const NavDrawer= createDrawerNavigator(
-  {
-    Welcome: WelcomeScreen,
-    Login: LoginScreen,
-    SignUp: SignUpScreen,
-    Home: HomeScreen,
-    Profile: ProfileScreen,
-    Settings: SettingsScreen
-  },
-    {
-    initialRouteName: 'Welcome',
-    unmountInactiveRoutes: true,
-    defaultNavigationOptions:{
-    //Title shows on the header of the app
-    title: 'Trace',
-    //(
-  //     <Image source={require('./src/images/Trace-3D.png')}/>
-  // ),
-      headerStyle: {
-        backgroundColor: '#202020'
-      },
-      headerTintColor: '#fff',
+{
+  Welcome: WelcomeScreen,
+  Login: LoginScreen,
+  SignUp: SignUpScreen,
+  Home: HomeScreen,
+  Profile: ProfileScreen,
+  Settings: SettingsScreen
+},
+{
+  //Set the Welcome Page as the first page of the app
+  initialRouteName: 'Welcome',
+  unmountInactiveRoutes: true,
+
+  defaultNavigationOptions:{
+  //Title shows on the header of the app
+  title: 'Trace',
+  //(
+//     <Image source={require('./src/images/Trace-3D.png')}/>
+// ),
+    headerStyle: {
+      backgroundColor: '#202020'
     },
-    contentComponent: DrawerMenu
-     }
+    headerTintColor: '#fff',
+  },
+  contentComponent: DrawerMenu
+}
+
 );
 
 const Navigator = createStackNavigator(
@@ -51,3 +57,49 @@ const Navigator = createStackNavigator(
 
 const Container = createAppContainer(Navigator);
 export default Container;
+
+
+/*
+//Loading screen
+const AuthLoadingScreen = (props) => {
+  // _loadData = async () => {
+  //   const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+  //   console.log(isLoggedIn);
+  //   props.navigation.navigate(isLoggedIn !== '1' ? 'Auth' : 'App')
+  // }
+  useEffect( () => {
+    async function _loadData() {
+      const isLoggedIn = await AsyncStorage.getItem('isLoggedIn');
+      console.log(isLoggedIn);
+      // props.navigation.navigate(isLoggedIn !== '1' ? 'Auth' : 'App');
+      props.navigation.navigate('Auth'); //comment this and uncomment line above after _logout has been implemented
+    }
+    _loadData();
+  });
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size='large' color='#0000ff'/>
+      <StatusBar barStyle='dark-content'/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+export default createAppContainer(createStackNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: Root,
+    Auth: AuthStack 
+  },
+  {
+    initialRouteName: 'AuthLoading'
+  }
+));
+*/
+

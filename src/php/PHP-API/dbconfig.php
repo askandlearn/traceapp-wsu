a4d5cec2fb643135d05cc646fdf7201a88722f5a
Mohammed Hamza <gh3173@wayne.edu>
2020-09-30 09:22:10 -0400

    php config files

---
+++ src/php/PHP-API/dbconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define host
+$hostname = "localhost";
+
+//Define database name
+$databasename = "api";
+
+//Define database username - leave blank
+$hostuser = "root";
+
+//Define database password - leave this blank
+$hostpass = "";
+
+?>

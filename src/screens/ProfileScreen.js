92c575ab7f003e5cfa9c58e8ac395f351bf56718
brinurmi <ev5443@wayne.edu>
2020-11-17 18:18:47 -0500

    Dyanmic birthdate on profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -302,7 +302,7 @@ const ProfileScreen = (props) => {
               onPress={()=> {setShowModalDate(!showModalDate)}}>Submit</Text>
              </View>
           </Modal>
-          <Text style={styles.content} onPress={()=> {setShowModalDate(!showModalDate)}}>mm/dd/yyyy</Text>
+          <Text style={styles.content} onPress={()=> {setShowModalDate(!showModalDate)}}>{currentUser.birthdate}</Text>
           </TouchableOpacity>
           <View style={styles.contentBorder} />
           {/*  ~~~~~~~~  Add section title and padding back later ~~~~~~~~~

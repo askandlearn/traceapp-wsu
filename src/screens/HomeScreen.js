2b20ba5640db0b6e4a511d953052b1afbd039120
Zachary Taylor <zataylor@wayne.edu>
2020-11-18 16:55:02 -0500

    Made yellow dashed line wider

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -138,7 +138,7 @@ const HomeScreen = ({navigation}) => {
               samples={1}
               // labels={"Fair"}
               style={{
-                data: {stroke: "#F7EF00", strokeDasharray: "4,4", strokeWidth: 1},
+                data: {stroke: "#F7EF00", strokeDasharray: "12,6", strokeWidth: 2},
               }}
             />
             <VictoryLine
@@ -146,7 +146,7 @@ const HomeScreen = ({navigation}) => {
               samples={1}
               // lables={["","Good"]}
               style={{
-                data: {stroke: "#00C019", strokeDasharray: "5,5", strokeWidth: 1}
+                data: {stroke: "#00C019", strokeDasharray: "6,6", strokeWidth: 1}
               }}
             />
             <VictoryBar

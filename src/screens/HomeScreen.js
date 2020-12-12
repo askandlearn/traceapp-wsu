c2b9bfabe8d0168560a7c9b5cb1185b1278e4c7d
Zachary Taylor <zataylor@wayne.edu>
2020-12-11 23:32:35 -0500

    merge commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,130 @@ import DeprecatedViewPropTypes from 'react-native/Libraries/DeprecatedPropTypes/
 import Header from '../components/Header-Component';
 import HealthDashboard from './HealthDashboardScreen';
 
+<<<<<<< Updated upstream
 const HomeScreen = ({navigation}) => {
+=======
+import Toast from 'react-native-simple-toast';
+import {connect} from 'react-redux';
+import { usePrevious } from '../hooks/usePrevious';
+
+const date = new Date();
+const strDay = (day) => {  
+  switch(day){
+    case 0: return 'Sun';
+    case 1: return 'Mon';
+    case 2: return 'Tue';
+    case 3: return 'Wed';
+    case 4: return 'Thu';
+    case 5: return 'Fri';
+    case 6: return 'Sat';
+    default: return 'Err';
+  }
+}
+
+
+
+const calDate = [];
+
+var thisDay=date.getDate();
+if(date.getDate() < 10){
+    var temp = "0";
+    thisDay = temp.concat(thisDay);
+  }
+var format = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + thisDay;
+calDate.push({
+  date: format,
+  score: Math.random() * 100,
+});
+
+var thisDay = date.getDate();
+var thisMonth = date.getMonth() + 1;
+var thisYear = date.getFullYear();
+var formatStr = '';
+for(var i = 1; i < 30; i++) {
+  if(thisDay == 1 && thisMonth == 1){
+    thisYear--;
+    thisMonth = 12;
+    thisDay = new Date(thisYear, thisMonth, 0).getDate();
+  } else if(thisDay == 1){
+    thisMonth--;
+    thisDay = new Date(thisYear, thisMonth, 0).getDate();
+  } else {
+    thisDay--;
+  }
+
+  if(thisDay < 10){
+    var temp = "0";
+    thisDay = temp.concat(thisDay);
+  }
+  var format = thisYear+ "-" + thisMonth + "-" + thisDay
+  
+  calDate.push({
+    date: format,
+    score: Math.floor(Math.random() * 100),
+  });
+
+  formatStr = formatStr.concat("\n" + format + " " + calDate[i].score);
+}
+
+const data = [
+  {day: strDay((date.getDay() + 1) % 7), score: calDate[6].score},
+  {day: strDay((date.getDay() + 2) % 7), score: calDate[5].score},
+  {day: strDay((date.getDay() + 3) % 7), score: calDate[4].score},
+  {day: strDay((date.getDay() + 4) % 7), score: calDate[3].score},
+  {day: strDay((date.getDay() + 5) % 7), score: calDate[2].score},
+  {day: strDay((date.getDay() + 6) % 7), score: calDate[1].score},
+  {day: strDay(date.getDay()), score: calDate[0].score},
+];
+
+var calData = {};
+
+for(const i in calDate) {
+  calData.[calDate[i].date] =  {
+    textColor: calDate[i].score > 70 || calDate[i].score < 40 && calDate[i].score != 0 ? 'white' : 'black',
+    color: calDate[i].score > 70 ? 'rgba(0,150,0,.8)': calDate[i].score >= 40 ? 'rgba(255,255,0,.6)' : calDate[i].score > 0 ? 'rgba(255,0,0,.8)' : 'white',
+    opacity: .4,
+    startingDay: true,
+    endingDay: true,
+  }
+}
+
+const sharedAxisStyles = {
+  axisLabel: {
+    margin: 30,
+    padding: 30,
+    fontSize: 15,
+    fontStyle: 'italic',
+  },
+};
+
+//redux states to props
+function mapStateToProps(state){
+  return{
+    isConnected : state.BLE['isConnected'],
+  };
+}
+
+
+const HomeScreen = (props) => {
+  //Toast for when the device disconnects
+  const {isConnected} = props
+  const prev = usePrevious(isConnected)
+  
+  useEffect(() => {
+    function showToast(){
+      if(prev === true && isConnected === false){
+        Toast.showWithGravity('Device has disconnected. Attempting to reconnect...', Toast.LONG, Toast.BOTTOM);
+      }
+    }
+
+    showToast()
+  }, [isConnected])
+  //End Toast
+
+  const [stats, setStats] = useState('week');
+  const [status, setStatus] = useState('true');
+>>>>>>> Stashed changes
   return (
     <View style={styles.container}>
       <Header openDrawer={navigation.openDrawer} />
---
+++ src/screens/RealTimeScreen.js
@@ -0,0 +1,277 @@
+import React, {useContext, useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import Header from '../components/Header-Component';
+import RTData from '../components/RTData';
+import {KeyboardAvoidingScrollView} from 'react-native-keyboard-avoiding-scroll-view';
+import {connect} from 'react-redux';
+import {stopTransaction, updateMetric} from '../actions';
+import {UserContext} from '../contexts/UserContext';
+import Toast from 'react-native-simple-toast';
+import { usePrevious } from '../hooks/usePrevious';
+import ModalComponent from '../components/Modal-Component';
+
+//require module
+var RNFS = require('react-native-fs');
+
+var check = true;
+
+const serviceUUID = '0000f80d-0000-1000-8000-00805f9b34fb'
+//transaction id for monitoring data
+const transactionID = 'monitor_metrics'
+
+//Get the bometric data from the device
+const mapStateToProps = state => ({
+  pnn50: state.DATA['pnn50'],
+  hrv: state.DATA['hrv'],
+  connectedDevice: state.BLE['connectedDevice'],
+  metrics: state.BLE['metrics'], //[0: time, 1: bpm, 2: ibi, 3: pamp, 4: damp, 5: ppg, 6: dif, 7: digout, 8: skintemp, 9: accelx,10: '/n'] size: 11
+  recordings: state.BLE['recordings'],
+  isConnected : state.BLE['isConnected'],
+  busy: state.BLE['busy'],
+  currTest: state.BLE['currTest']
+})
+
+//Control the display of data on the Real-Time page
+const mapDispatchToProps = dispatch => ({
+  updateMetric: (timeout, label) => dispatch(updateMetric(timeout, label)),
+  stopTransaction: ID => dispatch(stopTransaction(ID)),
+})
+
+const RealTimeScreen = (props) => {
+  //Toast for when the device disconnects
+  const {isConnected} = props
+  const prev = usePrevious(isConnected)
+  useEffect(() => {
+    function showToast(){
+      if(prev === true && isConnected === false){
+        Toast.showWithGravity('Device has disconnected. Attempting to reconnect...', Toast.LONG, Toast.BOTTOM);
+      }
+    }
+    showToast()
+  }, [isConnected])
+  //End Toast
+  //const user = useContext(UserContext)
+
+  //Start displaying the data on the page
+  const onStart = async () => {
+    if(isConnected == true){
+      props.updateMetric(undefined, 'RT');
+    }
+    else {
+      alert("TRACE device not connected.\n\n Connect your device and try again");
+    }
+  }
+  //Stop displaying the data on the page
+  const onStop = async () => {
+    console.log('Cancelling transaction...')
+    props.stopTransaction(transactionID);
+    setVisible(true)
+  }
+  const [visible, setVisible] = useState(true)
+
+  return (
+    <View behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    style={styles.container}>
+      <Header openDrawer={props.navigation.openDrawer}/>
+      {/* Get the survey after stopping */}
+      <ModalComponent visible={visible} setVisible={setVisible}/>
+
+      <Text style={styles.title}>Real-Time Data</Text>
+
+      {/* Start and stop buttons to control the data */}
+      <KeyboardAvoidingScrollView style={styles.bodyMain}>
+        <TouchableOpacity style={[styles.button, {backgroundColor: props.busy ? 'gray' : '#ff0000'}]} onPress={() => onStart()} disabled={props.busy}>
+          <Text style={styles.buttonText}>Start</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, {backgroundColor: !isConnected || !props.busy || props.currTest!='RT' ? 'gray' : '#ff0000'}]} onPress={() => onStop()} disabled={!isConnected || !props.busy || props.currTest!='RT'}>
+          <Text style={styles.buttonText}>Stop</Text>
+        </TouchableOpacity> 
+
+        {/* Call the component that displays the biometric data */}
+        <View testID="Data" style={styles.wrapper}>
+          <RTData></RTData>
+        </View>   
+      </KeyboardAvoidingScrollView> 
+    </View>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps) (RealTimeScreen);
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+              STYLE SHEET
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+const styles = StyleSheet.create({
+  bodyMain:{
+    marginTop:35,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignContent: 'center',
+  },
+  inputFields: {
+    backgroundColor: '#FFFFFF',
+    marginHorizontal: '10%',
+    marginVertical: 10,
+    padding: 10,
+    fontWeight: 'bold',
+    opacity: 0.4,
+    borderRadius: 3,
+  },
+  title: {
+    alignSelf: 'center',
+    color: '#242852',
+    fontWeight: 'bold',
+    fontSize: 35,
+    marginTop:25,
+    paddingTop:65,
+    shadowColor: '#000000',
+    shadowOffset: {width: .5, height: 1},
+    shadowOpacity: 0,
+    shadowRadius: 1,
+    elevation: 1,
+    ...Platform.select({
+      ios: {
+        fontFamily: 
+        'AppleSDGothicNeo-Bold'
+      },
+    }),
+  },
+  valueTitle: {
+    fontWeight: 'bold',
+    marginHorizontal: '8.5%',
+    marginTop: '3%',
+    width: 80,
+    height: 25,
+    alignSelf: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+  },
+  valueButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: '10%',
+    marginBottom: '1%',
+    borderRadius: 65,
+    borderWidth: 1,
+    width: 65,
+    height: 65,
+    borderColor: 'rgba(0,0,0,0.3)',
+    backgroundColor: 'rgba(255,255,255,1)',
+    shadowColor: '#000000',
+    shadowOffset: {width: 0, height: 3},
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  valueText: {
+    color: '#000000',
+  },
+  button: {
+    alignItems: 'center',
+    marginHorizontal: '10%',
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    paddingVertical:10,
+    borderRadius: 20,
+    backgroundColor: '#ff0000',
+    shadowColor: '#000000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  header: {
+    width: '100%',
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  ASTfigure: {
+    width: 210,
+    height: 214,
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  wrapper: {
+     height:600,
+     backgroundColor:'#ffffff', 
+   },
+   slide1: {
+     height:'100%',
+     paddingHorizontal:'10%',
+     justifyContent: 'center',
+     alignItems: 'center',
+     color: '#000000',
+     fontSize: 20,
+   },
+   slideTitles:{
+    alignSelf: 'center',
+    color: '#202020',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+   },
+   slide2: {
+     height:'100%',
+     paddingVertical:'10%',
+     paddingHorizontal:'10%',
+     alignItems: 'center',
+   },
+
+  steps: {
+    color: '#000000',
+    fontSize: 15,
+  },
+  centeredView: {
+    height: '90%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: '10%',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});

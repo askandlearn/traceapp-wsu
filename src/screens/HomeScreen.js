1b8c0e1927780abdec3c5f88416218cc5426b4ca
Mohammed Hamza <gh3173@wayne.edu>
2020-12-12 02:09:03 -0500

    fixed merge changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -12,110 +12,10 @@ import {VictoryBar, VictoryChart, VictoryAxis, VictoryTooltip, VictoryVoronoiCon
 import DropDownPicker from 'react-native-dropdown-picker';
 import Svg from 'react-native-svg';
 import {Calendar} from 'react-native-calendars';
-
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const HomeScreen = ({navigation}) => {
-=======
-=======
->>>>>>> cbf643e7a768a7a75f1ee9b81f6a9a97da4716e0
 import Toast from 'react-native-simple-toast';
 import {connect} from 'react-redux';
 import { usePrevious } from '../hooks/usePrevious';
 
-const date = new Date();
-const strDay = (day) => {  
-  switch(day){
-    case 0: return 'Sun';
-    case 1: return 'Mon';
-    case 2: return 'Tue';
-    case 3: return 'Wed';
-    case 4: return 'Thu';
-    case 5: return 'Fri';
-    case 6: return 'Sat';
-    default: return 'Err';
-  }
-}
-
-
-
-const calDate = [];
-
-var thisDay=date.getDate();
-if(date.getDate() < 10){
-    var temp = "0";
-    thisDay = temp.concat(thisDay);
-  }
-var format = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + thisDay;
-calDate.push({
-  date: format,
-  score: Math.random() * 100,
-});
-
-var thisDay = date.getDate();
-var thisMonth = date.getMonth() + 1;
-var thisYear = date.getFullYear();
-var formatStr = '';
-for(var i = 1; i < 30; i++) {
-<<<<<<< HEAD
-=======
-
->>>>>>> cbf643e7a768a7a75f1ee9b81f6a9a97da4716e0
-  if(thisDay == 1 && thisMonth == 1){
-    thisYear--;
-    thisMonth = 12;
-    thisDay = new Date(thisYear, thisMonth, 0).getDate();
-  } else if(thisDay == 1){
-    thisMonth--;
-    thisDay = new Date(thisYear, thisMonth, 0).getDate();
-  } else {
-    thisDay--;
-  }
-
-  if(thisDay < 10){
-    var temp = "0";
-    thisDay = temp.concat(thisDay);
-  }
-  var format = thisYear+ "-" + thisMonth + "-" + thisDay
-  
-  calDate.push({
-    date: format,
-    score: Math.floor(Math.random() * 100),
-  });
-
-  formatStr = formatStr.concat("\n" + format + " " + calDate[i].score);
-}
-
-const data = [
-  {day: strDay((date.getDay() + 1) % 7), score: calDate[6].score},
-  {day: strDay((date.getDay() + 2) % 7), score: calDate[5].score},
-  {day: strDay((date.getDay() + 3) % 7), score: calDate[4].score},
-  {day: strDay((date.getDay() + 4) % 7), score: calDate[3].score},
-  {day: strDay((date.getDay() + 5) % 7), score: calDate[2].score},
-  {day: strDay((date.getDay() + 6) % 7), score: calDate[1].score},
-  {day: strDay(date.getDay()), score: calDate[0].score},
-];
-
-var calData = {};
-
-for(const i in calDate) {
-  calData.[calDate[i].date] =  {
-    textColor: calDate[i].score > 70 || calDate[i].score < 40 && calDate[i].score != 0 ? 'white' : 'black',
-    color: calDate[i].score > 70 ? 'rgba(0,150,0,.8)': calDate[i].score >= 40 ? 'rgba(255,255,0,.6)' : calDate[i].score > 0 ? 'rgba(255,0,0,.8)' : 'white',
-    opacity: .4,
-    startingDay: true,
-    endingDay: true,
-  }
-}
-
-const sharedAxisStyles = {
-  axisLabel: {
-    margin: 30,
-    padding: 30,
-    fontSize: 15,
-    fontStyle: 'italic',
-  },
-};
 
 //redux states to props
 function mapStateToProps(state){
@@ -136,17 +36,103 @@ const HomeScreen = (props) => {
         Toast.showWithGravity('Device has disconnected. Attempting to reconnect...', Toast.LONG, Toast.BOTTOM);
       }
     }
-
     showToast()
   }, [isConnected])
   //End Toast
 
+  const date = new Date();
+  const strDay = (day) => {  
+    switch(day){
+      case 0: return 'Sun';
+      case 1: return 'Mon';
+      case 2: return 'Tue';
+      case 3: return 'Wed';
+      case 4: return 'Thu';
+      case 5: return 'Fri';
+      case 6: return 'Sat';
+      default: return 'Err';
+    }
+  }
+
+
+
+  const calDate = [];
+
+  var thisDay=date.getDate();
+  if(date.getDate() < 10){
+      var temp = "0";
+      thisDay = temp.concat(thisDay);
+    }
+  var format = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + thisDay;
+  calDate.push({
+    date: format,
+    score: Math.random() * 100,
+  });
+
+  var thisDay = date.getDate();
+  var thisMonth = date.getMonth() + 1;
+  var thisYear = date.getFullYear();
+  var formatStr = '';
+  for(var i = 1; i < 30; i++) {
+    if(thisDay == 1 && thisMonth == 1){
+      thisYear--;
+      thisMonth = 12;
+      thisDay = new Date(thisYear, thisMonth, 0).getDate();
+    } else if(thisDay == 1){
+      thisMonth--;
+      thisDay = new Date(thisYear, thisMonth, 0).getDate();
+    } else {
+      thisDay--;
+    }
+
+    if(thisDay < 10){
+      var temp = "0";
+      thisDay = temp.concat(thisDay);
+    }
+    var format = thisYear+ "-" + thisMonth + "-" + thisDay
+    
+    calDate.push({
+      date: format,
+      score: Math.floor(Math.random() * 100),
+    });
+
+    formatStr = formatStr.concat("\n" + format + " " + calDate[i].score);
+  }
+
+  const data = [
+    {day: strDay((date.getDay() + 1) % 7), score: calDate[6].score},
+    {day: strDay((date.getDay() + 2) % 7), score: calDate[5].score},
+    {day: strDay((date.getDay() + 3) % 7), score: calDate[4].score},
+    {day: strDay((date.getDay() + 4) % 7), score: calDate[3].score},
+    {day: strDay((date.getDay() + 5) % 7), score: calDate[2].score},
+    {day: strDay((date.getDay() + 6) % 7), score: calDate[1].score},
+    {day: strDay(date.getDay()), score: calDate[0].score},
+  ];
+
+  var calData = {};
+
+  for(const i in calDate) {
+    calData.[calDate[i].date] =  {
+      textColor: calDate[i].score > 70 || calDate[i].score < 40 && calDate[i].score != 0 ? 'white' : 'black',
+      color: calDate[i].score > 70 ? 'rgba(0,150,0,.8)': calDate[i].score >= 40 ? 'rgba(255,255,0,.6)' : calDate[i].score > 0 ? 'rgba(255,0,0,.8)' : 'white',
+      opacity: .4,
+      startingDay: true,
+      endingDay: true,
+    }
+  }
+
+  const sharedAxisStyles = {
+    axisLabel: {
+      margin: 30,
+      padding: 30,
+      fontSize: 15,
+      fontStyle: 'italic',
+    },
+  };
+
   const [stats, setStats] = useState('week');
   const [status, setStatus] = useState('true');
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> cbf643e7a768a7a75f1ee9b81f6a9a97da4716e0
+
   return (
     <View
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
